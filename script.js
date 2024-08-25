bd629deaf5bb9fa1f54cda7737fc813477f0b527
Abir Bhattacharjee <145211003+abir-bhattacharjee@users.noreply.github.com>
2024-08-25 14:53:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-setInterval(() => {
-    d = new Date();
-    htime = d.getHours();
-    mtime = d.getMinutes();
-    stime = d.getSeconds();
-    hrotation = 30*htime + mtime/2;
-    mrotation = 6*mtime;
-    srotation = 6*stime;
+const tickSound = new Audio("clock-tick.mp3");
 
-    hour.style.transform = `rotate(${hrotation}deg)`;
-    minute.style.transform = `rotate(${mrotation}deg)`;
-    second.style.transform = `rotate(${srotation}deg)`;
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
+
+setInterval(() => {
+  const d = new Date();
+  const htime = d.getHours();
+  const mtime = d.getMinutes();
+  const stime = d.getSeconds();
+
+  const hrotation = 30 * htime + mtime / 2;
+  const mrotation = 6 * mtime;
+  const srotation = 6 * stime;
+
+  hour.style.transform = `rotate(${hrotation}deg)`;
+  minute.style.transform = `rotate(${mrotation}deg)`;
+  second.style.transform = `rotate(${srotation}deg)`;
+
+  tickSound.currentTime = 0;
+  tickSound.play();
 }, 1000);
